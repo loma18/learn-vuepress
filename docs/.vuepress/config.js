554f1702c547b7b1405b66dd9571866d740ac41a
mazhengxian <0027015405@iwhalecloud.com>
2021-12-30 14:24:17 +0800

    init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  title: 'TypeScript4 文档',
+  description: 'TypeScript4 最新官方文档翻译',
+  theme: 'reco',
+  base: '/learn-vuepress/',
+  themeConfig: {
+    nav: [
+      { text: '首页', link: '/' },
+      {
+        text: '贤哥的 JavaScript 博客',
+        items: [
+          { text: 'Github', link: 'https://github.com/loma18' },
+        ]
+      }
+    ]
+  },
+}
